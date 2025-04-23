9ccacd531c23549d8138b083014dc151cb6fdfc6
Hexenos <113744313+Hexenos@users.noreply.github.com>
2025-04-23 02:48:50 -0400

    Add per round function nested within a per game function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,11 @@
 function getHumanChoice() {
     // Prompt user input
-    let choice = prompt("Choose an option: rock, paper, or scissors");
+    let choice = prompt("Choose an option: Rock, Paper, or Scissors").toLowerCase();
     // Convert user input to value
     return(choice);
     // Return value
 }
 
-console.log(getHumanChoice());
 
 function getComputerChoice() {
     let choiceRandom = Math.random();
@@ -21,7 +20,41 @@ function getComputerChoice() {
     
 }
 
-console.log(getComputerChoice());
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    function playRound(choice, choiceRandom) {
+        console.log(choice);
+        console.log(choiceRandom);
+         // Determine value hierarchy
+         if (choice === choiceRandom) {
+             console.log("It's a tie!");
+         } else if (choice === "rock" && choiceRandom === "scissors") {
+             humanScore++;
+             console.log("You win! Rock beats Scissors.");
+         } else if (choice === "scissors" && choiceRandom === "paper") {
+             humanScore++;
+             console.log("You win! Scissors beats Paper.");
+         } else if (choice === "paper" && choiceRandom === "rock") {
+             humanScore++;
+             console.log("You win! Paper beats Rock.");
+         } else {
+             computerScore++;
+             console.log(`You lose! ${choiceRandom.charAt(0).toUpperCase() + choiceRandom.slice(1)} beats ${choice.charAt(0).toUpperCase() + choice.slice(1)}`);
+         } 
+         // Compare both parameters
+         // Output winner of round
+         // Increase score for winning side
+     }
+
+     for (let i = 0; i < 5 ;i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+     }
+}
+
+playGame();
